ea64a247ec7ad53cdd5f83af327455e4293fba31
John Wayne Lee <redraiderglobal@yahoo.com>
2026-08-26 13:44:15 -0500

    Support Vercel and GitHub Pages deployments

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  base: "/roadtrip-app/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "github-pages" ? "/roadtrip-app/" : "/",
   plugins: [react()],
-});
+}));
